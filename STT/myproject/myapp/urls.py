c94cff7b0970672094b180a8f50622b7243dae6d
kanggyurin <165642959+kanggyurin@users.noreply.github.com>
2024-06-21 07:50:04 +0900

    Delete STT/myproject directory

--- STT/myproject/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('index/', views.upload_audio, name='upload_audio'),
-    path('speech-to-text/', views.speech_to_text, name='speech_to_text'),
-]
